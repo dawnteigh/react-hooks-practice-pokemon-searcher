d4dd65ed04c76970c2abcd5f60c2f88222b9e4d2
Donte Ladatto <donte279@gmail.com>
2022-04-17 16:47:12 -0500

    commit tree f1a185357360633923a3932ba806cbb10a31a2f6

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-function Search() {
+function Search({ onSearchChange }) {
+ 
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input onChange={onSearchChange} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
